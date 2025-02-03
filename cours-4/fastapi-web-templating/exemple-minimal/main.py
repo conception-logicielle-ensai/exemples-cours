ee70a561394857a137ac354be973536e17d3d800
Antoine Brunetti <antoine.brunetti@insee.fr>
2025-02-03 15:41:33 +0100

    ajout d'un path parameter a l'exemple minimal fastapitemplating

--- cours-4/fastapi-web-templating/exemple-minimal/main.py
+++ cours-4/fastapi-web-templating/exemple-minimal/main.py
@@ -5,9 +5,9 @@ from fastapi.responses import HTMLResponse
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
-@app.get("/", response_class=HTMLResponse)
-async def home(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request, "message": "Bienvenue sur FastAPI avec SSR !"})
+@app.get("/{chemin}", response_class=HTMLResponse)
+async def home(request: Request,chemin:str):
+    return templates.TemplateResponse("index.html", {"request": request, "message": f"Bienvenue sur FastAPI avec SSR {chemin}!"})
 
 if __name__ == "__main__":
     import uvicorn
