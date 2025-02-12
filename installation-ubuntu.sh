6c34674d0a91316220ff8a02f53004103fa3a175
salut <Vous@exemple.com>
2025-02-12 19:55:26 +0100

    un script unique d'install pour les gouverner tous

--- installation-ubuntu.sh
+++ installation-ubuntu.sh
@@ -54,7 +54,43 @@ print_section "Installation de chromium"
 sudo apt-get install chromium-browser
 print_section "Installation de chromedriver"
 curl -Lo ~/downloads/chromedriver.zip https://storage.googleapis.com/chrome-for-testing-public/133.0.6943.53/linux64/chromedriver-linux64.zip
-unzip ~/downloads/chromedriver.zip -d ~/downloads
+unzip -o ~/downloads/chromedriver.zip -d ~/downloads
 mv ~/downloads/chromedriver-linux64/chromedriver ~/downloads/chromedriver
 sudo chmod +x ~/downloads/chromedriver
 sudo mv ~/downloads/chromedriver /usr/bin/chromedriver
+print_section "desinstallation de firefox via snap, reinstallation de firefox via apt
+echo "kill"
+pkill firefox
+sudo snap remove firefox
+sudo install -d -m 0755 /etc/apt/keyrings
+wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
+echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | sudo tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null
+echo "
+Package: *
+Pin: origin packages.mozilla.org
+Pin-Priority: 1000
+
+Package: firefox*
+Pin: release o=Ubuntu
+Pin-Priority: -1' | sudo tee /etc/apt/preferences.d/mozilla"
+echo "installation de firefox"
+sudo apt update && sudo apt remove firefox
+sudo apt install firefox
+
+print_section "installation de docker"
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo service docker restart
