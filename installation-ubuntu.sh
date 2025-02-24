c1fc22aeda95407601dbcea959ca796b5a9947a5
salut <Vous@exemple.com>
2025-02-24 10:39:22 +0100

    ajout script kubectl

--- installation-ubuntu.sh
+++ installation-ubuntu.sh
@@ -102,3 +102,15 @@ sudo apt-get update
 
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo service docker restart
+
+print_section "installation de kubectl"
+
+curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.16.0/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+
+windowsUser=$1
+
+mkdir -p ~/.kube
+ln -sf "/mnt/c/users/$windowsUser/.kube/config" ~/.kube/config
+
