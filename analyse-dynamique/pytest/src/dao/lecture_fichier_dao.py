8f643e617804f3a6a3bfb1991e168bbcc4b80efb
ragatzino <antoinebrunetti@gmail.com>
2026-01-18 19:06:27 +0100

    :screw: Ruff apply

--- analyse-dynamique/pytest/src/dao/lecture_fichier_dao.py
+++ analyse-dynamique/pytest/src/dao/lecture_fichier_dao.py
@@ -2,6 +2,7 @@ import logging
 from pathlib import Path
 from typing import Iterable
 import requests
+
 logger = logging.getLogger(__name__)
 
 
@@ -10,10 +11,12 @@ class LectureFichierDAO:
     Classe pour lire un fichier local ou distant (HTTP/HTTPS).
     """
 
-    def _is_file_on_a_remote(self,chemin_fichier_str):
-        return chemin_fichier_str.startswith("http://") or chemin_fichier_str.startswith("https://")
-    
-    def _lire_lignes_remote_file(self, url:str):
+    def _is_file_on_a_remote(self, chemin_fichier_str):
+        return chemin_fichier_str.startswith(
+            "http://"
+        ) or chemin_fichier_str.startswith("https://")
+
+    def _lire_lignes_remote_file(self, url: str):
         try:
             r = requests.get(url, stream=True)
             r.raise_for_status()
@@ -21,14 +24,15 @@ class LectureFichierDAO:
             for raw_line in r.iter_lines(decode_unicode=True):
                 yield raw_line
         except Exception as e:
-                logger.error(f"Impossible de télécharger '{url}': {e}")
-                return  # générateur vide pour pouvoir continuer le process
+            logger.error(f"Impossible de télécharger '{url}': {e}")
+            return  # générateur vide pour pouvoir continuer le process
 
-
-    def _lire_lignes_local_file(self, base_dir:str, chemin_fichier_str:str):
+    def _lire_lignes_local_file(self, base_dir: str, chemin_fichier_str: str):
         chemin_fichier = Path(base_dir) / chemin_fichier_str
         if not chemin_fichier.exists():
-            logger.error(f"Le fichier '{chemin_fichier_str}' n'existe pas dans le repertoire {base_dir}")
+            logger.error(
+                f"Le fichier '{chemin_fichier_str}' n'existe pas dans le repertoire {base_dir}"
+            )
             return  # générateur vide pour pouvoir continuer le process
         try:
             with open(chemin_fichier, "r", encoding="utf-8") as f:
@@ -36,8 +40,10 @@ class LectureFichierDAO:
         except Exception as e:
             logger.error(f"Impossible de lire '{chemin_fichier_str}': {e}")
             return  # générateur vide pour pouvoir continuer le process
-    
-    def lire_lignes(self, base_dir:str, chemin_fichier_str: str) -> Iterable[str]:
+
+    def lire_lignes(self, base_dir: str, chemin_fichier_str: str) -> Iterable[str]:
         if self._is_file_on_a_remote(chemin_fichier_str=chemin_fichier_str):
             return self._lire_lignes_remote_file(url=chemin_fichier_str)
-        return self._lire_lignes_local_file(base_dir=base_dir, chemin_fichier_str=chemin_fichier_str)
+        return self._lire_lignes_local_file(
+            base_dir=base_dir, chemin_fichier_str=chemin_fichier_str
+        )
--- analyse-dynamique/pytest/src/main.py
+++ analyse-dynamique/pytest/src/main.py
@@ -3,10 +3,10 @@ from src.view.compteur_view import CompteurView
 import logging
 
 logging.basicConfig(
-    filename="app.log",       # nom du fichier de log
-    filemode="a",             # "w" pour écraser à chaque lancement, "a" pour append
-    level=logging.INFO,       # niveau de log minimal
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    filename="app.log",  # nom du fichier de log
+    filemode="a",  # "w" pour écraser à chaque lancement, "a" pour append
+    level=logging.INFO,  # niveau de log minimal
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
 )
 
 compteur_view = CompteurView(os.getenv("BASE_DIR", "./samples"))
